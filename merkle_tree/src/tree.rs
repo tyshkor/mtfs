531797516838ee0f4223aed61bab1d67a4dfe9f9
tyshkor <tyshko1@gmail.com>
2023-05-30 18:23:09 +0200

    binary tree

---
+++ merkle_tree/src/tree.rs
@@ -0,0 +1,49 @@
+use ring::digest::{Algorithm, Digest};
+use crate::hashing::{Hashing, Hashable};
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum BinaryTree<T> {
+    Empty {
+        hash: Vec<u8>,
+    },
+    Leaf {
+        hash: Vec<u8>,
+        value: T,
+    },
+    Node {
+        hash: Vec<u8>,
+        left: Box<BinaryTree<T>>,
+        right: Box<BinaryTree<T>>,
+    },
+}
+
+impl<T> BinaryTree<T> {
+    pub fn empty(hash: Digest) -> Self {
+        BinaryTree::Empty {
+            hash: hash.as_ref().into(),
+        }
+    }
+
+    pub fn new(hash: Digest, value: T) -> Self {
+        BinaryTree::Leaf {
+            hash: hash.as_ref().into(),
+            value,
+        }
+    }
+
+    pub fn new_leaf(algo: &'static Algorithm, value: T) -> BinaryTree<T>
+    where
+        T: Hashable,
+    {
+        let hash = algo.hash_leaf(&value);
+        BinaryTree::new(hash, value)
+    }
+
+    pub fn hash(&self) -> &Vec<u8> {
+        match *self {
+            BinaryTree::Empty { ref hash } => hash,
+            BinaryTree::Leaf { ref hash, .. } => hash,
+            BinaryTree::Node { ref hash, .. } => hash,
+        }
+    }
+}
